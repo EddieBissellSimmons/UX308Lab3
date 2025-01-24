2016c1a14e58df823399eb13031d2657c4b66c13
EddieBissellSimmons <144965190+EddieBissellSimmons@users.noreply.github.com>
2025-01-24 12:42:36 -0500

    Update suggestC_spec.js

--- spec/suggestC_spec.js
+++ spec/suggestC_spec.js
@@ -1,10 +1,10 @@
-import { suggestClothing } from "../suggestClothing.js";
+import { clothingSuggestion } from "../clothingSuggestion.js";
 
 describe("test suggestClothing", function() {
     it("tests humidity >60 and temperature >4", function() {
         let humidity = 100;
         let temp = 20;
-        let clothing = suggestClothing(humidity, temp)
+        let clothing = clothingSuggestion(humidity, temp)
         expect(clothing).toBe("rain jacket");
     });
   });
